c08f058f03cc16f7f66bdda18b3ddb4b3d23f282
Kathy Vo <kathyvo143@gmail.com>
2016-01-28 11:01:02 -0800

    app code

---
+++ function.js
@@ -0,0 +1,12 @@
+
+//creating image array of food
+var imgArray = new Array();
+
+imgArray[0] = new Image(); 
+imgArray[0].src = "food/1.jpeg"; 
+
+imgArray[1] = new Image(); 
+imgArray[1].src = "food/2.jpeg"; 
+
+imgArray[2] = new Image(); 
+imgArray[2].src = "food/3.jpeg"; 
