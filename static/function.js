e4a67ab721216859fa092f2da717982dc78c7f76
Kathy Vo <kathyvo143@gmail.com>
2016-01-28 19:55:55 -0800

    changed menu options, disabled dropdown box, and got random food slideshow working

--- static/function.js
+++ static/function.js
@@ -1,9 +1,28 @@
 $(document).ready(function() {
 
+	var prev = 0;
+
 	$('button').click(function() {
 
+		var rand = Math.floor(Math.random() * 3);
 
-        $('img').attr('src', 'http://whalebonemag.com/wp-content/uploads/2015/08/DSC_4647-Edit-1050x701.jpg');
+		while (rand === prev) { 
+			rand = Math.floor(Math.random() * 3);
+		}
+		prev = rand;
 
+		if (rand === 0) {
+			
+        	$('img').attr('src', 'http://whalebonemag.com/wp-content/uploads/2015/08/DSC_4647-Edit-1050x701.jpg');
+        	//prev = 0;
+    	}
+    	else if (rand === 1) {
+    		$('img').attr('src', 'http://i.imgur.com/GWoeeaN.jpg');
+    		//prev = 1;
+    	}
+    	else { 
+    		$('img').attr('src', 'http://spoilednyc.com/wp-content/uploads/2015/07/23/final-5056.jpg');
+    		//prev = 2;
+    	}
 	});
 })
