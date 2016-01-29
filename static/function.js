53444d15a02465b2536337ae2ea19ef27964c7ac
Kathy Vo <kathyvo143@gmail.com>
2016-01-28 16:23:14 -0800

    image change

---
+++ static/function.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+	$('button').click(function() {
+
+
+        $('img').attr('src', 'http://whalebonemag.com/wp-content/uploads/2015/08/DSC_4647-Edit-1050x701.jpg');
+
+	});
+})
