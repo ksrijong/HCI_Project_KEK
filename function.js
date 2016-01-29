24edf949eb091a11745acffe64463215b824a85d
Kathy Vo <kathyvo143@gmail.com>
2016-01-28 16:11:15 -0800

    change size

--- function.js
+++ function.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
-	$('button').on('click', function() {
+	$('#spin').live('click', function() {
 
-		//if($('img').attr('src') === 'http://placehold.it/350x150')
-            $('img').attr('src', 'http://whalebonemag.com/wp-content/uploads/2015/08/DSC_4647-Edit-1050x701.jpg');
+
+        $('.dish').attr('src', 'http://whalebonemag.com/wp-content/uploads/2015/08/DSC_4647-Edit-1050x701.jpg');
 
 	});
 })
