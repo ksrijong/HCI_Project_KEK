7612f14a347c3611b890e036437538c99634f537
Kirk Srijongsirikul <srijongsirikul.kirk@gmail.com>
2016-02-04 23:25:20 +0000

    testing food images

--- static/function.js
+++ static/function.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
 
     //creating food image array
-    var foodArr = new Array(); 
-    var burger = "../food/burger.jpg";
-    var pizza = '../food/pizza.jpg';
-    var sushi = '../food/sushi.jpg';
-    var steak = '../food/steak.jpg';
+    var foodArr = new Array();
+    var burger = "food/burger.jpg";
+    var pizza = 'food/pizza.jpg';
+    var sushi = 'food/sushi.jpg';
+    var steak = 'food/steak.jpg';
     foodArr = [burger, pizza, sushi, steak];
 
     var i = 0;
     //moves to next image
-    $("#next").click(function() { 
+    $("#next").click(function() {
         $('#bmk_confirm').css('visibility', 'hidden');
         console.log("right clicked!");
         if (i < foodArr.length - 1) {
@@ -18,18 +18,18 @@ $(document).ready(function() {
             $('img').attr('src', foodArr[i]);
             i++;
         }
-        else if (i == foodArr.length - 1) { 
+        else if (i == foodArr.length - 1) {
             $('img').attr('src', foodArr[i]);
             $("#end").css('visibility', 'visible');
         }
-        else { 
+        else {
 
         }
         console.log(i); // for testing purposes
     });
 
     //clicking left will go back to previous
-    $("#prev").click(function() { 
+    $("#prev").click(function() {
         $('#bmk_confirm').css('visibility', 'hidden');
         console.log("left clicked!");
         if (i > 0) {
@@ -37,18 +37,18 @@ $(document).ready(function() {
             $("#end").css('visibility', 'hidden');
             $('img').attr('src', foodArr[i]);
         }
-        else if (i == 0){ 
+        else if (i == 0){
             $('img').attr('src', foodArr[i]);
             $("#end").css('visibility', 'visible');
         }
-        else { 
+        else {
 
-        }       
+        }
         console.log(i);
     });
-	
+
     //receive confirmation that you bookmarked dish
-    $('#bmk').click(function() { 
+    $('#bmk').click(function() {
         $('#bmk_confirm').css('visibility', 'visible');
     })
 })
