5bd2e6f32208113d9e6665e20a0cd8f8105b85db
Kirk Srijongsirikul <srijongsirikul.kirk@gmail.com>
2016-02-10 00:40:10 +0000

    linked bookmarks from the homepage

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ if ('development' == app.get('env')) {
 }
 
 // Add routes here
+//app.get('/', index.view);
 app.get('/bookmarks', bookmarks.view);
 // Example route
 // app.get('/users', user.list);
